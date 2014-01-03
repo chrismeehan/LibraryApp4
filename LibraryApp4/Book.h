32980c92b7ddc9b040b9c19d1790230bc70125e5
chrismeehan <chrismeehan360@hotmail.com>
2014-01-03 08:26:06 -0800

    I added a folder, and a Book class

---
+++ LibraryApp4/Book.h
@@ -0,0 +1,13 @@
+//
+//  Book.h
+//  LibraryApp4
+//
+//  Created by Chris Meehan on 1/3/14.
+//  Copyright (c) 2014 Chris Meehan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Book : NSObject
+
+@end
---
+++ LibraryApp4/Book.m
@@ -0,0 +1,13 @@
+//
+//  Book.m
+//  LibraryApp4
+//
+//  Created by Chris Meehan on 1/3/14.
+//  Copyright (c) 2014 Chris Meehan. All rights reserved.
+//
+
+#import "Book.h"
+
+@implementation Book
+
+@end
