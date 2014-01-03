33f039daabdfab62481adffcf2497df1720b2bb0
chrismeehan <chrismeehan360@hotmail.com>
2014-01-03 08:30:37 -0800

    Now i have a Book class and a Test class

---
+++ LibraryApp4/TestFile.m
@@ -0,0 +1,13 @@
+//
+//  TestFile.m
+//  LibraryApp4
+//
+//  Created by Chris Meehan on 1/3/14.
+//  Copyright (c) 2014 Chris Meehan. All rights reserved.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+@end
